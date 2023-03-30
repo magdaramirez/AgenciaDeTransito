3249a298ff7733aa815f9bb25aed005d59b3e755
MichellCedano <michell.cedano233230@potros.itson.edu.mx>
2023-03-30 14:46:13 -0700

    Correcion del mapeo de clases

---
+++ AgenciaDeTransito/src/main/java/org/itson/implementaciones/ConstantesGUI.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.itson.implementaciones;
+
+/**
+ *
+ * @author Magda - Michell
+ */
+public class ConstantesGUI {
+// Tipos de años de vigencia de licencias ----
+    
+public static int ANIO1 = 1;
+public static int ANIO2 = 2;
+public static int ANIO3 = 3;
+
+// Tipos de precios de licencias
+
+public static int COSTOLICENCIA_NORMAL1 = 600;
+public static int COSTOLICENCIA_NORMAL2 = 900;
+public static int COSTOLICENCIA_NORMAL3 = 1100;
+public static int COSTOLICENCIA_DISCAPACITADO1 = 200;
+public static int COSTOLICENCIA_DISCAPACITADO2 = 500;
+public static int COSTOLICENCIA_DISCAPACITADO3 = 700;
+
+//Vehiculos ----------------------------------
+
+//automoviles nuevos
+public static int COSTOVEHICULO_NUEVO = 1500;
+//automoviles usados
+public static int COSTOVEHICULO_USADO = 1000;
+
+}
